81724c6f5483726da5ce8526fdaa7f04b37e9971
Lukasz Baranowski <lukaszb48@gmail.com>
2021-11-01 09:54:29 +0100

    Create Global.js

---
+++ src/components/styles/Global.js
@@ -0,0 +1,13 @@
+import { createGlobalStyles } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font-family: 'Poppins', sans-serif;
+  }
+`
+
+export default GlobalStyles;
