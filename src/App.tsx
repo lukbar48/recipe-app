75305f2b16210b63fc7dacf7cd9161ef5ef5749a
Lukasz Baranowski <lukaszb48@gmail.com>
2021-11-01 10:05:19 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
-import styled from 'styled-components'
+import styled, { ThemeProvider } from 'styled-components';
+import GlobalStyles from './components/styles/Global';
+import Container from './components/styles/Container.styled';
+import NavBar from './components/NavBar';
+
+const theme = {
+  colors: {
+    main: orange,
+    header: black,
+    text: grey,
+  },
+  mobile: 768px,
+}
 
 function App() {
+  
   return (
-    <Container>
-      <h1>Search recipe app</h1>
-    </Container>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <>
+<!--         <NavBar /> -->
+        <Container>
+          <h1>Search recipe app</h1>
+        </Container>
+       </>
+     </ThemeProvider>
   );
 }
 
